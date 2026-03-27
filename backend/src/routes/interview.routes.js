fd31b25ee520587125b2a0189b4d86989ed33b33
Sankalp Panchabhai <sankalppanchabhai5@gmail.com>
2026-03-27 19:32:46 +0530

    added the route for the interview process

---
+++ backend/src/routes/interview.routes.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const { authUser } = require("../middlewares/auth.middleware");
+// const { interview } = require()
+const interviewController = require("../controllers/interview.controllers")
+
+const interviewroute = express.Router();
+
+/**
+ * @route GET /api/description/interview 
+ * @description route to get the data from the db
+ * @access public  
+ */
+interviewroute.get("/description", interviewController.tempResult);
+
+/**
+ * @route POST /api/description/gemini
+ * @description route to get the data from the gemini API
+ * @access public
+ */
+// route.get("/description", authUser,)
+
+/**
+ * @route POST /api/description/gemini
+ * @description route to get the data from the gemini API
+ * @access public
+ */
+// route.get("/description", authUser,)
+
+module.exports = interviewroute
