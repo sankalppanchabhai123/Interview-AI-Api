264f38af07be960a153a9ef7d551eaa563992db4
Sankalp Panchabhai <sankalppanchabhai5@gmail.com>
2026-03-29 00:25:43 +0530

    added test gemini api

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 const app = require("./src/app");
 const connectDb = require("./src/config/database");
+const { tempResult: invokeGeminiAi } = require("./src/controllers/interview.controllers");
 
 connectDb();
+invokeGeminiAi();
 
 app.listen(3000, () => {
     console.log("server listening on port 3000")
