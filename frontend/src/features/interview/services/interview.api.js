0ae8febd7a7e0358ed40c4f176dc8774cbed85d8
Sankalp Panchabhai <sankalppanchabhai5@gmail.com>
2026-04-04 12:37:53 +0530

    added backend url

--- frontend/src/features/interview/services/interview.api.js
+++ frontend/src/features/interview/services/interview.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000/api/interview",
+    baseURL: "https://interview-ai-api-backend.onrender.com/api/interview",
     withCredentials: true,
 });
 
