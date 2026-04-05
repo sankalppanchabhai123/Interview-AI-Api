344a8f02fba2c0e77c048ee43e4cc949ebcd08de
Sankalp Panchabhai <sankalppanchabhai5@gmail.com>
2026-04-05 16:47:58 +0530

    added versel.json

--- frontend/src/features/auth/services/auth.api.js
+++ frontend/src/features/auth/services/auth.api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-    baseURL: "https://interview-ai-api-eok2.onrender.com/api/auth",
+    baseURL: "http://localhost:3000/api/auth",
     withCredentials: true,
 });
 
