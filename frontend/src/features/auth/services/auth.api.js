58c7b2bb2c91f13973c6bf936837a78f7dadb6fa
Sankalp Panchabhai <147154199+sankalppanchabhai123@users.noreply.github.com>
2026-04-07 00:11:55 +0530

    Update auth.api.js

--- frontend/src/features/auth/services/auth.api.js
+++ frontend/src/features/auth/services/auth.api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-    baseURL: "http://localhost:3000/api/auth",
+    baseURL: "https://interview-ai-api-backend.onrender.com/api/auth",
     withCredentials: true,
 });
 
