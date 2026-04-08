eef79c8cc2cc881de78918eb1924dd5bf5c942ac
Sankalp Panchabhai <147154199+sankalppanchabhai123@users.noreply.github.com>
2026-04-09 02:18:44 +0530

    Update API base URL for authentication service

--- frontend/src/features/auth/services/auth.api.js
+++ frontend/src/features/auth/services/auth.api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-    baseURL: "https://interview-ai-api-backend.onrender.com/api/auth",
+    baseURL: "https://interview-test-eosin-three.vercel.app/api/auth",
     // baseURL: "http://localhost:3000/api/auth",
     withCredentials: true,
 });
